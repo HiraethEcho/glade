abad616907584350ca286e23fb9f023d5366557e
arch14 <wyz2016zxc@outlook.com>
2025-09-25 18:58:24 +0800

    btn js works

--- assets/js/nav-button.js
+++ assets/js/nav-button.js
@@ -1,5 +1,20 @@
-document.getElementById('siteNavBtn').addEventListener('click', function() {
-    const nav = document.getElementById('siteNav');
-    console.log('fuck');
-    nav.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', function() {
+    const sideBarBtn = document.querySelector('.sidebar-btn');
+    const sideBar = document.querySelector('.sidebar');
+
+    const siteNav = document.querySelector('.site-nav');
+    const siteNavBtn = document.querySelector('.site-nav-btn');
+    
+    sideBarBtn.addEventListener('click', function() {
+        sideBar.classList.toggle('active');
+    });
+
+    siteNavBtn.addEventListener('click', function() {
+        siteNav.classList.toggle('active');
+        if (siteNavBtn.textContent === '▼') {
+            siteNavBtn.textContent = '▲';
+        } else {
+            siteNavBtn.textContent = '▼';
+        }
+    });
 });
