6f338cb68809c249628056fb3eba2687feeab61c
arch14 <wyz2016zxc@outlook.com>
2025-09-25 16:45:13 +0800

    custom css

---
+++ assets/js/nav-button.js
@@ -0,0 +1,5 @@
+document.getElementById('siteNavBtn').addEventListener('click', function() {
+    const nav = document.getElementById('siteNav');
+    console.log('fuck');
+    nav.classList.toggle('active');
+});
