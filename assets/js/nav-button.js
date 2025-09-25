8c054b2c0fc5b311203c4bfd2d38f7d68de0f376
arch14 <wyz2016zxc@outlook.com>
2025-09-26 01:39:29 +0800

    update classes etc

--- assets/js/nav-button.js
+++ assets/js/nav-button.js
@@ -1,20 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     const sideBarBtn = document.querySelector('.sidebar-btn');
     const sideBar = document.querySelector('.sidebar');
+    sideBarBtn.addEventListener('click', function() {
+        sideBar.classList.toggle('active');
+        if (sideBarBtn.textContent === '▼') {
+            sideBarBtn.textContent = '▲';
+        } else {
+            sideBarBtn.textContent = '▼';
+        }
+    });
 
     const siteNav = document.querySelector('.site-nav');
     const siteNavBtn = document.querySelector('.site-nav-btn');
-    
-    sideBarBtn.addEventListener('click', function() {
-        sideBar.classList.toggle('active');
-    });
-
     siteNavBtn.addEventListener('click', function() {
         siteNav.classList.toggle('active');
-        if (siteNavBtn.textContent === '▼') {
-            siteNavBtn.textContent = '▲';
-        } else {
-            siteNavBtn.textContent = '▼';
-        }
     });
 });
