23f40269787ceb19fea7083539a81b2cb3ec2eda
arch14 <wyz2016zxc@outlook.com>
2025-09-28 15:22:29 +0800

    update icons etc

--- assets/js/nav-button.js
+++ assets/js/nav-button.js
@@ -3,16 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const sideBar = document.querySelector('.sidebar');
     sideBarBtn.addEventListener('click', function() {
         sideBar.classList.toggle('active');
-        if (sideBarBtn.textContent === '▼') {
-            sideBarBtn.textContent = '▲';
-        } else {
-            sideBarBtn.textContent = '▼';
-        }
     });
 
     const siteNav = document.querySelector('.site-nav');
     const siteNavBtn = document.querySelector('.site-nav-btn');
     siteNavBtn.addEventListener('click', function() {
         siteNav.classList.toggle('active');
+        if (siteNavBtn.textContent === '▼') {
+            siteNavBtn.textContent = '◀';
+        } else {
+            siteNavBtn.textContent = '▼';
+        }
     });
 });
